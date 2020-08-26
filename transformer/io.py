fbd2dd4832d6993c714d4afb0f1ddd7973330c45
adamvinueza <adamvinueza@pm.me>
2020-08-26 16:19:37 -0600

    overwrite rename

--- transformer/io.py
+++ transformer/io.py
@@ -4,8 +4,11 @@ from typing import Any
 
 class Writer():
     """Writer streams data to a destination file."""
-    def __init__(self, dest: str, fs: AbstractFileSystem, ow: bool) -> None:
-        if fs.exists(dest) and not ow:
+    def __init__(self,
+                 dest: str,
+                 fs: AbstractFileSystem,
+                 overwrite: bool) -> None:
+        if fs.exists(dest) and not overwrite:
             raise FileExistsError(
                 f"File {dest} exists and overwrite is not set")
         self.fout = fs.open(dest, 'wb')
