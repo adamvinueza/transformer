4e85b5ceedef777e32e3bf65b70a9654407d5209
Adam Vinueza <adamvinueza@pm.me>
2020-08-27 11:32:50 -0600

    Add comment on how Writer works

--- examples/disemvowel/disemvowel.py
+++ examples/disemvowel/disemvowel.py
@@ -6,6 +6,8 @@ def disemvowel(rdr, wr) -> None:
         for i in range(len(content)):
             if content[i] not in 'aeiouAEIOU':
                 b += bytes(content[i], 'utf-8')
+        # We need to call the Writer even if there is no data, so
+        # it knows to close the output stream.
         wr(b)
         if not content:
             break
