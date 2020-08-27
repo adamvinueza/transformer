2f99f9748c21f1966f84b6c474131a94a5da731a
adamvinueza <adamvinueza@pm.me>
2020-08-26 18:15:13 -0600

    mild refactoring

--- examples/disemvowel/disemvowel.py
+++ examples/disemvowel/disemvowel.py
@@ -2,12 +2,11 @@ def disemvowel(rdr, wr) -> None:
     bufsize = 1024
     while True:
         content = rdr.read(bufsize).decode('utf-8')
-        content_lc = content.lower()
         if not content:
             break
         b = bytes()
         for i in range(len(content)):
-            if content_lc[i] not in 'aeiou':
+            if content[i] not in 'aeiouAEIOU':
                 b += bytes(content[i], 'utf-8')
         wr(b)
 
