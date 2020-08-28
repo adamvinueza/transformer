28916fea77ec1c8ddc2c6266fac957c15c6cd3af
adamvinueza <adamvinueza@pm.me>
2020-08-28 17:40:41 -0600

    added first draft of encrypt/decrypt and skeleton of main

---
+++ examples/gpg/local/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import sys
+sys.path.append('..')
+import gpg
+
+from transformer.transform import Transform
+from fsspec.implementations.local import LocalFileSystem
+
+if __name__ == '__main__':
+    pass
---
+++ examples/gpg/s3/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import sys
+sys.path.append('..')
+import gpg
+
+from transformer.transform import Transform
+import s3fs
+
+if __name__ == '__main__':
+    pass
