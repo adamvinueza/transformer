e61b2a9a8dec5f5c8c6af58e6fbf3806ca29104f
Adam Vinueza <adamvinueza@pm.me>
2020-09-14 09:05:17 -0600

    bump version to 0.3 (#27)
    * bump version to 0.3
    
    * fix setup.py broken with bad edit

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='transformer',
-      version='0.2',
+      version='0.3',
       description='Library for transforming data via streaming files',
       url='http://github.com/adamvinueza/transformer',
       author='Adam Vinueza',
       author_email='adamvinueza@pm.me',
       license='MIT',
       packages=['transformer'],
-      install_requires=['fsspec==0.8.0'],
+      install_requires=['fsspec==0.8.0', 'mypy-extensions==0.4.3'],
       zip_safe=False)
