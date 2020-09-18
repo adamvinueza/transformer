6c60497773438988063abf0d6029177f2bd010a0
adamvinueza <adamvinueza@pm.me>
2020-09-17 22:29:06 -0600

    type annotations for fs.py

--- transformer/fs.py
+++ transformer/fs.py
@@ -1,21 +1,22 @@
 from configparser import ConfigParser
-from s3fs import S3FileSystem
-from fsspec.implementations.local import LocalFileSystem
-from fsspec.implementations.sftp import SFTPFileSystem
+from s3fs import S3FileSystem  # type: ignore
+from fsspec import AbstractFileSystem  # type: ignore
+from fsspec.implementations.local import LocalFileSystem  # type: ignore
+from fsspec.implementations.sftp import SFTPFileSystem  # type: ignore
 import os.path
 
 
-def S3FS(profile_name):
+def S3FS(profile_name: str) -> AbstractFileSystem:
     creds_path = os.path.join(
         os.path.expanduser('~'),
         '.aws/credentials'
     )
     conf = ConfigParser()
     conf.read(creds_path)
-    aws_access_key_id = conf.get(profile, 'aws_access_key_id'),
-    aws_secret_access_key = conf.get(profile, 'aws_secret_access_key'),
-    aws_session_token = conf.get(profile, 'aws_session_token')
-    return s3fs.S3FileSystem(
+    aws_access_key_id = conf.get(profile_name, 'aws_access_key_id'),
+    aws_secret_access_key = conf.get(profile_name, 'aws_secret_access_key'),
+    aws_session_token = conf.get(profile_name, 'aws_session_token')
+    return S3FileSystem(
         anon=False,
         key=aws_access_key_id,
         secret=aws_secret_access_key,
@@ -23,11 +24,15 @@ def S3FS(profile_name):
     )
 
 
-def LocalFS():
+def LocalFS() -> AbstractFileSystem:
     return LocalFileSystem()
 
 
-def SFTPFS(host, temppath, username, password, port):
+def SFTPFS(host: str,
+           temppath: str,
+           username: str,
+           password: str,
+           port: int) -> AbstractFileSystem:
     return SFTPFileSystem(host,
                           temppath,
                           {
